197b2395c3bf67696e315dd0ae5f40cdef2fd899
chuly92 <julietagallego92@gmail.com>
2022-05-11 22:25:24 -0300

    3rd Version - Responsive Added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,63 +1,177 @@
-import React from 'react';
-import logo from '../assets/reactlogo.png';
+import React, { useState } from 'react';
+import logo from '../assets/pikachu.png';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { Avatar, Tooltip } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+
 
 export const NavBar = () => {
-  return (
-    <Box>
-      <AppBar position="static" sx={{ bgcolor: 'black' }}>
-        <Toolbar>
 
-          <IconButton edge="start" aria-label="menu">
-            <img
-              src={logo} alt="logo"
-              style={{ maxWidth: 30, maxHeight: 30 }}
-            />
+  //Define menu values
+  const pages = ['Home', 'Store', 'New Incomes'];
+  const settings = ['Account', 'Orders', 'Logout'];
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (e) => {
+    setAnchorElNav(e.currentTarget);
+  };
+  const handleOpenUserMenu = (e) => {
+    setAnchorElUser(e.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ bgcolor: 'black' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+
+          {/* Render icon page in full-size window (widht up to 900px) */}
+          <IconButton
+            edge="start" aria-label="menu"
+            sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
+          >
+            <img src={logo} alt="logo" style={{ maxWidth: 50, maxHeight: 50 }} />
           </IconButton>
 
-          <Typography
-            variant="h6" component="div" color="white"
-            sx={{ flexGrow: 1 }}
+          {/* Render title store in full-size window (width up to 900px) */}
+          <Typography variant="h5"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              color: 'red'
+            }}
           >
             PikaStore
           </Typography>
 
-          <Button
-            sx={{ textTransform: 'capitalize', fontSize: 16 }}
-            color="secondary"
+          {/* Render pages in responsive window (width down to 900px) */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton size="large" onClick={handleOpenNavMenu} color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          {/* Render icon page in responsive window (width down to 900px) */}
+          <IconButton
+            edge="start" aria-label="menu"
+            sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}
           >
-            Homepage
-          </Button>
+            <img src={logo} alt="logo" style={{ maxWidth: 40, maxHeight: 40 }} />
+          </IconButton>
 
-          <Button
-            sx={{ textTransform: 'capitalize', fontSize: 16 }}
-            color="secondary"
+          {/* Render title in responsive window (width down to 900px) */}
+          <Typography variant="h6"
+            sx={{
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              color: 'red',
+              textDecoration: 'none'
+            }}
           >
-            Store</Button>
+            PikaStore
+          </Typography>
 
-          <Button
-            sx={{ textTransform: 'capitalize', fontSize: 16 }}
-            color="secondary"
+          {/* Render pages in full-size window (width up to 900px) */}
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: 'center',
+              alignContent: 'center', 
+              display: { xs: 'none', md: 'flex' }
+            }}
           >
-            New Incomes</Button>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ 
+                  color: 'white', 
+                  display: 'block', 
+                  textTransform: 'capitalize', 
+                  fontSize: 16, 
+                  letterSpacing: '.2rem',
+                  mx: 2
+                }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
 
+          {/* Render user settings for all screens */}
+          <Box sx={{ flexGrow: 0 }}>
 
-          <Box >
-            <Tooltip title="Open settings">
-              <IconButton sx={{ p: 0, ml: 10 }}>
-                <Avatar alt="user" />
-              </IconButton>
-            </Tooltip>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Julieta" src="/" />
+            </IconButton>
+
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
           </Box>
 
         </Toolbar>
-      </AppBar>
-    </Box>
+      </Container>
+    </AppBar>
   );
 };
